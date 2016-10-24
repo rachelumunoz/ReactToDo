c9f1b50627e6830e3fe3d039cee9b01aa0adae1f
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-23 21:41:25 -0700

    Adds static todo list

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,16 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var { Route, Router, IndexRoute, hashHistory} = require('react-router')
 
+var ToDoApp = require('ToDoApp')
+
 //Load Foundation
 $(document).foundation()
 //app css
 require('style!css!sass!applicationStyles')
 
 
-ReactDOM.render(<p>Boilerplate 3 project</p>
-  , document.getElementById('app'))
+ReactDOM.render(
+  <ToDoApp/>, 
+  document.getElementById('app')
+)
     
