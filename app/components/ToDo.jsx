3a5474541c3374f4ae79b19d24969af4a1075cbc
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-25 20:50:51 -0700

    Adds timestamps

--- app/components/ToDo.jsx
+++ app/components/ToDo.jsx
@@ -1,15 +1,24 @@
 var React = require('react')
-
+var moment = require('moment')
 var ToDo = React.createClass({
   render: function(){
-    var {text, id, completed } = this.props
-
+    var {text, id, completed, createdAt, completedAt } = this.props
+    var renderDate = ()=>{
+      var message = 'Created at '
+      var timeStamp = createdAt
+      if (completed){
+        message = 'Completed at '
+        timeStamp = completedAt
+      }
+      return message + moment.unix(timeStamp).format('MMM Do YYYY @ h:mm a')
+    }
     return (
       <div onClick={()=>{
         this.props.onToggle(id)
       }}>
         <input type="checkbox" checked={completed}/>
-        {text}
+         <p>{text}</p> 
+         <p>{renderDate()}</p>  
       </div>
     )
   }
--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -19,13 +19,17 @@ describe('ToDoApp', ()=>{
     toDoApp.handleNewToDo(todoText)
 
     expect(toDoApp.state.todos[0].text).toBe(todoText)
+    //Expected createdAt to be a number
+    expect(toDoApp.state.todos[0].createdAt).toBeA('number')
   })
 
   it('should toggle completed value when handleToggle called', ()=>{
     var toDoData = {
       id: 11,
       text: 'A test',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     }
     var toDoApp = TestUtils.renderIntoDocument(<ToDoApp/>)
     toDoApp.setState({todos: [toDoData]})
@@ -33,6 +37,23 @@ describe('ToDoApp', ()=>{
     expect(toDoApp.state.todos[0].completed).toBe(false)
     toDoApp.handleToggle(11)
     expect(toDoApp.state.todos[0].completed).toBe(true)
+    expect(toDoApp.state.todos[0].completedAt).toBeA('number')
+  })
 
+  it('should remove compledAt time when toggled', ()=>{
+    var toDoData = {
+      id: 11,
+      text: 'A test',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    }
+    var toDoApp = TestUtils.renderIntoDocument(<ToDoApp/>)
+    toDoApp.setState({todos: [toDoData]})
+
+    expect(toDoApp.state.todos[0].completed).toBe(true)
+    toDoApp.handleToggle(11)
+    expect(toDoApp.state.todos[0].completed).toBe(false)
+    expect(toDoApp.state.todos[0].completedAt).toNotExist()
   })
 })
